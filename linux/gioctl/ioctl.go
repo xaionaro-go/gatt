efb0e52cc02fb314549f931e819fd740e9c98814
photostorm <eephoto45@gmail.com>
2019-10-04 22:19:42 -0400

    got working for mips

--- linux/gioctl/ioctl.go
+++ linux/gioctl/ioctl.go
@@ -1,21 +1,12 @@
 package gioctl
 
-import "syscall"
+import (
+	"syscall"
+)
 
 const (
-	typeBits      = 8
 	numberBits    = 8
-	sizeBits      = 14
-	directionBits = 2
-
-	typeMask      = (1 << typeBits) - 1
-	numberMask    = (1 << numberBits) - 1
-	sizeMask      = (1 << sizeBits) - 1
-	directionMask = (1 << directionBits) - 1
-
-	directionNone  = 0
-	directionWrite = 1
-	directionRead  = 2
+	typeBits      = 8
 
 	numberShift    = 0
 	typeShift      = numberShift + numberBits
