6757a09982a5f2450aa08e0d60a94e8d777a1188
photostorm <eephoto45@gmail.com>
2020-07-02 16:55:12 -0400

    prevent close channel error

--- linux/util/pool.go
+++ linux/util/pool.go
@@ -1,8 +1,11 @@
 package util
 
+import "sync"
+
 type BytePool struct {
 	pool  chan []byte
 	width int
+	mu    sync.RWMutex
 }
 
 func NewBytePool(width int, depth int) *BytePool {
@@ -13,10 +16,20 @@ func NewBytePool(width int, depth int) *BytePool {
 }
 
 func (p *BytePool) Close() {
+	p.mu.Lock()
 	close(p.pool)
+	p.pool = nil
+	p.mu.Unlock()
 }
 
 func (p *BytePool) Get() (b []byte) {
+	p.mu.RLock()
+	defer p.mu.RUnlock()
+
+	if p.pool == nil {
+		return nil
+	}
+
 	select {
 	case b = <-p.pool:
 	default:
@@ -26,6 +39,13 @@ func (p *BytePool) Get() (b []byte) {
 }
 
 func (p *BytePool) Put(b []byte) {
+	p.mu.RLock()
+	defer p.mu.RUnlock()
+
+	if p.pool == nil {
+		return
+	}
+
 	select {
 	case p.pool <- b:
 	default:
