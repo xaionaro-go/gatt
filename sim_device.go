a66dd7c968e9253bd5c74d04dfa88f3f4296eeb3
Dmitrii Okunev <xaionaro@dx.center>
2025-12-30 18:45:49 +0000

    Ugly hacks to make the Simulated device to work for djictl

--- sim_device.go
+++ sim_device.go
@@ -3,6 +3,7 @@ package gatt
 import (
 	"context"
 	"errors"
+	"sync"
 )
 
 const (
@@ -12,15 +13,24 @@ const (
 type simDevice struct {
 	deviceHandler
 
-	service        *Service
-	advertisedName string
+	service          *Service
+	advertisedName   string
+	manufacturerData []byte
+
+	subscribersLocker sync.Mutex
+	subscribers       map[uint16]func(*Characteristic, []byte, error)
 }
 
 func NewSimDeviceClient(service *Service, advertisedName string) *simDevice {
 	return &simDevice{
 		service:        service,
 		advertisedName: advertisedName,
+		subscribers:    make(map[uint16]func(*Characteristic, []byte, error)),
 	}
+}
+
+func (d *simDevice) SetManufacturerData(b []byte) {
+	d.manufacturerData = b
 }
 
 func (d *simDevice) Start(
@@ -74,12 +84,28 @@ func (d *simDevice) SetServices(ctx context.Context, ss []*Service) error {
 }
 
 func (d *simDevice) Scan(ctx context.Context, ss []UUID, dup bool) error {
+	// Fix: support nil/empty ss to allow discovery by djictl
+	if len(ss) == 0 {
+		go d.peripheralDiscovered(
+			ctx,
+			&simPeripheral{d},
+			&Advertisement{
+				LocalName:        d.advertisedName,
+				ManufacturerData: d.manufacturerData,
+			},
+			0,
+		)
+		return nil
+	}
 	for _, s := range ss {
 		if s.Equal(d.service.UUID()) {
 			go d.peripheralDiscovered(
 				ctx,
 				&simPeripheral{d},
-				&Advertisement{LocalName: d.advertisedName},
+				&Advertisement{
+					LocalName:        d.advertisedName,
+					ManufacturerData: d.manufacturerData,
+				},
 				0,
 			)
 		}
@@ -109,6 +135,26 @@ func (d *simDevice) Option(o ...Option) error {
 	return ErrMethodNotSupported
 }
 
+// Added helper for notifications
+func (d *simDevice) SendNotification(vh uint16, b []byte) {
+	d.subscribersLocker.Lock()
+	defer d.subscribersLocker.Unlock()
+	f, ok := d.subscribers[vh]
+	if ok {
+		// Find characteristic
+		var char *Characteristic
+		for _, c := range d.service.Characteristics() {
+			if c.VHandle() == vh {
+				char = c
+				break
+			}
+		}
+		if char != nil {
+			go f(char, b, nil)
+		}
+	}
+}
+
 type simPeripheral struct {
 	d *simDevice
 }
@@ -118,7 +164,7 @@ func (p *simPeripheral) Device() Device {
 }
 
 func (p *simPeripheral) ID() string {
-	return "Sim ID"
+	return "00:11:22:33:44:55"
 }
 
 func (d *simDevice) ID() int {
@@ -134,6 +180,9 @@ func (p *simPeripheral) Services(ctx context.Context) []*Service {
 }
 
 func (p *simPeripheral) DiscoverServices(ctx context.Context, ss []UUID) ([]*Service, error) {
+	if len(ss) == 0 {
+		return []*Service{p.d.service}, nil
+	}
 	for _, s := range ss {
 		if s.Equal(p.d.service.UUID()) {
 			return []*Service{p.d.service}, nil
@@ -143,16 +192,19 @@ func (p *simPeripheral) DiscoverServices(ctx context.Context, ss []UUID) ([]*Ser
 }
 
 func (p *simPeripheral) DiscoverIncludedServices(ctx context.Context, ss []UUID, s *Service) ([]*Service, error) {
-	return nil, ErrMethodNotSupported
+	return nil, nil // Fix: don't return error
 }
 
 func (p *simPeripheral) DiscoverCharacteristics(ctx context.Context, cc []UUID, s *Service) ([]*Characteristic, error) {
+	if len(cc) == 0 {
+		return s.Characteristics(), nil
+	}
 	requestedUUIDs := make(map[string]bool)
 	for _, c := range cc {
 		requestedUUIDs[c.String()] = true
 	}
 	foundChars := make([]*Characteristic, 0)
-	for _, c := range p.d.service.Characteristics() {
+	for _, c := range s.Characteristics() {
 		if _, present := requestedUUIDs[c.UUID().String()]; present {
 			foundChars = append(foundChars, c)
 		}
@@ -161,7 +213,7 @@ func (p *simPeripheral) DiscoverCharacteristics(ctx context.Context, cc []UUID, 
 }
 
 func (p *simPeripheral) DiscoverDescriptors(ctx context.Context, d []UUID, c *Characteristic) ([]*Descriptor, error) {
-	return nil, ErrMethodNotSupported
+	return nil, nil // Fix: don't return error
 }
 
 func (p *simPeripheral) ReadCharacteristic(ctx context.Context, c *Characteristic) ([]byte, error) {
@@ -205,15 +257,30 @@ func (p *simPeripheral) WriteDescriptor(ctx context.Context, d *Descriptor, b []
 }
 
 func (p *simPeripheral) Subscribe(vh uint16, f func(*Characteristic, []byte, error)) {
-	panic(ErrMethodNotSupported)
+	// Fix: implement subscription
+	p.d.subscribersLocker.Lock()
+	p.d.subscribers[vh] = f
+	p.d.subscribersLocker.Unlock()
+
+	// Trigger notify handler if any
+	for _, c := range p.d.service.Characteristics() {
+		if c.VHandle() == vh {
+			if h := c.GetNotifyHandler(); h != nil {
+				go h.ServeNotify(context.Background(), Request{}, nil)
+			}
+			break
+		}
+	}
 }
 
 func (p *simPeripheral) SetNotifyValue(ctx context.Context, c *Characteristic, f func(*Characteristic, []byte, error)) error {
-	return ErrMethodNotSupported
+	p.Subscribe(c.VHandle(), f)
+	return nil
 }
 
 func (p *simPeripheral) SetIndicateValue(ctx context.Context, c *Characteristic, f func(*Characteristic, []byte, error)) error {
-	return ErrMethodNotSupported
+	p.Subscribe(c.VHandle(), f)
+	return nil
 }
 
 func (p *simPeripheral) ReadRSSI(ctx context.Context) int {
@@ -221,5 +288,5 @@ func (p *simPeripheral) ReadRSSI(ctx context.Context) int {
 }
 
 func (p *simPeripheral) SetMTU(ctx context.Context, mtu uint16) error {
-	return ErrMethodNotSupported
+	return nil // Fix: don't return error
 }
